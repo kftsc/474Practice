dfec71149980a104d642382432e23e642df4b158
kftsc <kftsc3@gmail.com>
2020-02-17 16:30:40 -0500

    third finish?

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("hellow world");
+console.log("hello world");
+$(function(){
+    //this code runs after page is fully loaded
+    //console.log("function1");
+
+    $("#go").on("click", function() {
+        //alert("from is submitted");
+        console.log("form is submitted");
+    });
+
+    
+    $("#go").click(function(event) {
+        event.preventDefault();
+        $('#jumbotron').addClass("d-block");
+        //$('#jumbotron').css('display', 'block');
+        //$('#jumbotron').show();
+        var firstName = document.getElementById("fname").value;
+        var lastName = document.getElementById("lname").value;
+        console.log(firstName + ", " + lastName);
+        $("#info").html(firstName + " " + lastName);
+        
+    });
+
+    
+    function myFunction() {
+        
+      }
+});
+
+
+
+
+    
